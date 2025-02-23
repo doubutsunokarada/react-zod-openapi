ef21cbe4eed6c16c2b06a52de9e23676c3d234b3
Chiaki Abe <c.abe@sonicmoov.com>
2025-02-23 21:06:58 +0900

    [chore]: update gitignore

--- packages/schema/dist/generator.d.ts
+++
@@ -1,9 +0,0 @@
-import { OpenAPIRegistry } from "@asteasolutions/zod-to-openapi";
-export declare const registry: OpenAPIRegistry;
-export declare const bearerAuth: {
-    name: string;
-    ref: {
-        $ref: string;
-    };
-};
-import "./routes/contact";
--- packages/schema/dist/parameters/pagination/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pagination = void 0;
-const zod_1 = require("zod");
-exports.pagination = zod_1.z.object({
-    limit: zod_1.z.number().int().multipleOf(10).default(10),
-    offset: zod_1.z.number().int().nonnegative().default(0),
-    sort: zod_1.z.enum(["asc", "desc"]).default("asc"),
-});
