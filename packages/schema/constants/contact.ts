a8f49a9105c0bb2cb653dc8538694af9ee8d9d20
Chiaki Abe <c.abe@sonicmoov.com>
2025-02-23 22:21:34 +0900

    [update]: update constants file

--- packages/schema/constants/contact.ts
+++ packages/schema/constants/contact.ts
@@ -14,4 +14,18 @@ export const CONTACT_TYPE = {
   { value: ContactTypeValues; label: string }
 >;
 
+// NOTE: IDだった場合の実装例
+
+// const CONTACT_TYPE_VALUES = [1, 2, 3] as const;
+// type ContactTypeValuesType = (typeof CONTACT_TYPE_VALUES)[number];
+
+// export const CONTACT_TYPE = {
+//   Product: { value: 1, label: "製品" },
+//   Technical: { value: 2, label: "技術" },
+//   General: { value: 3, label: "その他全般" },
+// } as const satisfies Record<
+//   ContactTypeKeys,
+//   { value: ContactTypeValuesType; label: string }
+// >;
+
 export const contactTypeSchema = z.enum(CONTACT_TYPE_NAMES);
